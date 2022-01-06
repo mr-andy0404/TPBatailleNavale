317bc3f49324219989d9fa5b64e148746ab516b5
Chongan <wca0404@gmail.com>
2022-01-06 11:09:21 +0100

    Modification du bateau et variable

--- Bateau.cpp
+++ Bateau.cpp
@@ -1,1 +1,17 @@
 #include "Bateau.h"
+
+//using namespace std;
+
+Bateau::Bateau(string _nom, int _type, Pos _positionInit, bool direction) {
+
+    nom = _nom;
+    type = _type;
+    position.push_back(_positionInit);
+    for (int i = 1; i <= type; i++) {
+        Pos newPos;
+        newPos.x = _positionInit.x + i * direction;
+        newPos.y = _positionInit.y + i * (1 - direction);
+        newPos.est_touche = false;
+        position.push_back(newPos);
+    }
+}
